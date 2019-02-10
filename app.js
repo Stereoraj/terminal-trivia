dd2929f3522fd5490d6ddb63f4effb75c7eb0779
rajesh <merajesh04@gmail.com>
2019-02-11 00:18:05 +0530

    Add column menu view for each questions

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
+const term = require("terminal-kit").terminal;
+
 const trivia = require("./trivia/trivia.js");
 
 // initialize the counter
 let counter = 0;
 let score = 0;
+let questionCollection = [];
 
 // fetch questions and answers
 const questions = trivia.getTrivia()
 .then((res) => {
-    res.map((res) => {
-        console.log(`asking question number ${counter + 1}`);
-        console.log(res.question);
-        counter = counter + 1;
-        console.log("***************");
-    });
+        questionCollection = res;
+        displayMenu(questionCollection[counter])
 })
 .catch((err) => {
     console.log("Error occurred : ", err);
 });
 
 
+
+const displayMenu = (questionObj) => {
+    console.log(`asking question number ${counter + 1}`);
+        counter = counter + 1;
+
+        term.cyan(questionObj.question);
+        const options = questionObj.answers;
+
+        term.singleColumnMenu( options , function( error , response ) {
+            term( '\n' ).eraseLineAfter.green(
+                "#%s selected: %s (%s,%s)\n" ,
+                response.selectedIndex ,
+                response.selectedText ,
+                response.x ,
+                response.y
+            );
+            if(counter < 10){
+                displayMenu(questionCollection[counter]);
+            }else{
+                process.exit() ;
+            }
+            
+        } ) ;
+}
     
