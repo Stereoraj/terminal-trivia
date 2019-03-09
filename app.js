b61933e9237996ef98c52d6b13652da094eb3fe4
rajesh <merajesh04@gmail.com>
2019-03-10 01:20:37 +0530

    Format display - colors, spaces, schemes

--- app.js
+++ app.js
@@ -41,9 +41,24 @@ async function loadGameSession(questionCollection){
 
     term.clear();
     
-    displayMsgFormatted("<<TERMINAL TRIVIA>>");
-    displayMsgFormatted("-------------------------");
-    displayMsgFormatted(`10 on 10 !! Winner Winner Chicken Dinner ....`);
+    // displayMsgFormatted("<<TERMINAL TRIVIA>>");
+    // displayMsgFormatted("-------------------------");
+    // displayMsgFormatted(`10 on 10 !! Winner Winner Chicken Dinner ....`);
+
+    term.colorRgbHex("#bd3b1b");
+    // term.bgColorRgbHex("#b9d870");
+    displayMsgFormatted("  ------------------------------- ");
+    term("\n");
+    displayMsgFormatted("  <<TERMINAL TRIVIA>>  ");
+    term("\n");
+    displayMsgFormatted("  ------------------------------- ");
+    term.bgDefaultColor();
+
+    term("\n\n");
+
+    term.colorRgbHex("#d8a800");
+    displayMsgFormatted(`10 on 10 !! Winner Winner Chicken Dinner .... \n\n`);
+    term.defaultColor();
 
     process.exit();
 }
@@ -54,7 +69,7 @@ async function displayMenu(questionObj){
     counter = counter + 1;
     term.bgColorRgbHex("#DBFF33");
     term.colorRgbHex("#0B3C49");
-    term(`| Question No : ${counter} / 10 |`);
+    term(`  Question No : ${counter} / 10  `);
     term.bgDefaultColor();
 
     term.moveTo(term.width - 11, 1);
@@ -66,7 +81,7 @@ async function displayMenu(questionObj){
     term("\n\n");
 
     // term.colorRgbHex("#FFFDFD");
-    term.colorRgbHex("#688E26");
+    term.colorRgbHex("#b9d870");
     term("* ",questionObj.question);
     const options = questionObj.answers;
     // term(questionObj.correct_answer);
@@ -78,28 +93,32 @@ async function displayMenu(questionObj){
     term.colorRgbHex("#FFFDFD");
     // term.bgColorRgbHex("#666B6A");
     // term.colorRgbHex("#A0EEC0");
+    term.colorRgbHex("#00ff44");
 
     var res = await term.singleColumnMenu(options).promise;
 
     //console.log(res);
     //console.log(questionObj);
 
+    term.defaultColor();
+    term.colorRgbHex("#fff");
+    term("\n\n");
+
     if(res.selectedText === questionObj.correct_answer){
-        term("Correct Answer");
+        term.bgBrightGreen(" Correct Answer !! ");
         score = score + 10;
     }else{
-        term("\n\n");
         term.bgColorRgbHex("#ff0000");
-        term(" Wrong Answer !! ");
+        term(" Wrong Answer !!    ");
         term.bgDefaultColor();
-        term("\n\n");
-        term("Correct Answer is : ", questionObj.correct_answer);
-        term("\n\n");
-        term(`Your score : ${score}\n`);
+        term("\n");
+        term.bgBrightBlue(" Correct Answer  -> ").bgBrightGreen(`  ${questionObj.correct_answer}  `);
+        term("\n");
+        term.bgBrightBlue(` Your Score      -> `).bgBrightYellow(`   ${score}  \n\n`);
         process.exit();
     }
-    term("\n");
-    term(">>>  PLEASE ANY KEY TO CONTINUE <<<");
+    term("\n\n");
+    term.bgYellow(">>>  PLEASE ANY KEY TO CONTINUE <<<");
 
     await require("./utilities/keyHandler").keyPress();
     
@@ -110,20 +129,38 @@ const welcomeInstruction = () => {
 
     // get the cursor position of the terminal to display message
     // the cursor position such that the message appears in the center
+    term.colorRgbHex("#bd3b1b");
+    // term.bgColorRgbHex("#b9d870");
+    displayMsgFormatted("  ----------------------------------- ");
+    term("\n");
+    displayMsgFormatted("  WELCOME TO TERMINAL TRIVIA  ");
+    term("\n");
+    displayMsgFormatted("  ----------------------------------- ");
+    term.bgDefaultColor();
 
-    displayMsgFormatted("WELCOME TO TERMINAL TRIVIA");
+    term("\n\n");
+
+    term.colorRgbHex("#d8a800");
     displayMsgFormatted("<< BASIC INSTRUCTION >>");
+    term("\n\n");
+
+    term.colorRgbHex("#b9d870");
     displayMsgFormatted(" * YOU WILL BE ASKED 10 QUESTIONS ");
+    term("\n");
     displayMsgFormatted(" * USE THE ARROW KEY TO NAVIGATE THE OPTIONS AND PRESS ENTER TO SELECT IT");
+    term("\n");
     displayMsgFormatted(" * YOU CAN PLAY THE GAME WHILE YOU GIVE THE CORRECT ANSWER");
+    term("\n\n");
+
+    term.colorRgbHex("#ee3b1b");
     displayMsgFormatted(" [[ PRESS ANY KEY TO CONTINUE ]]");
-    
+    term("\n");
 }
 
 const displayMsgFormatted = (msg) => {
     var cursorPos = require("./utilities/centerText").getCursorPosition(term, msg);
     term.column(cursorPos);
     term(msg);
-    term.nextLine(2);
+    //term.nextLine(2);
 }
 
